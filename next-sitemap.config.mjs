cf17d1a4fc1c6b39bfe6fada27b43362841adb15
zhugechina <zhugetd@126.com>
2025-03-29 04:04:15 +0800

    init

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -2,7 +2,7 @@
 
 /** @type {import('next-sitemap').IConfig} */
 export default {
-  siteUrl: "https://baseballbrosio.fun",
+  siteUrl: "https://ghiblistyleimagegenerator.cc",
   generateIndexSitemap: false,
   generateRobotsTxt: false,
   // output: "export", // Set static output here
